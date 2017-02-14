26d10b376269507fe7d03d0c569071254f6a2348
DarmiusX <tsmmers1@memphis.edu>
2017-02-14 14:19:25 -0600

    Updated to be able to compile without error

--- main.cpp
+++ main.cpp
@@ -7,9 +7,9 @@
 
 int atomic_distance (int atom_dist_x, int atom_dist_y, int atom_dist_z); //Kate's Prototype: defines atomic distance as combination of x, y, z coordinates
 
-int (main)
-{
-using nedit file...
-create new input "hannah's test changes"
+int main() {
+//using nedit file...
+//create new input "hannah's test changes"
+	return 0;
 }
 
