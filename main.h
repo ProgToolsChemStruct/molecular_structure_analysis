f33512e59b3230fe88c6c54ac5c45964966ed704
DarmiusX <tsmmers1@memphis.edu>
2017-02-20 00:51:01 -0600

    Started a header file for main (main.h)

---
+++ main.h
@@ -0,0 +1,16 @@
+//This is the header file for main.cpp
+
+#include <iostream>
+#include <string>
+#include <list>
+
+using namespace std;
+
+class gaussian_output 
+{
+private:
+  static const string HEADER_STRING;
+  static const string FOOTER_STRING;
+};
+
+
