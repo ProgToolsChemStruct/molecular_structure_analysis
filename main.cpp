b6814dec82564f9a6ded92c5de4317ba53f17609
DarmiusX <tsmmers1@memphis.edu>
2017-02-20 00:57:48 -0600

    Added code to scan an input file for keywords

--- main.cpp
+++ main.cpp
@@ -1,15 +1,40 @@
 #include <iostream>
+#include <string>
+#include <fstream>
 
+#include "main.h"
 
-//Thomas' Prototypes
+using namespace std;
 
-//Hannah's Prototypes
-
-int atomic_distance (int atom_dist_x, int atom_dist_y, int atom_dist_z); //Kate's Prototype: defines atomic distance as combination of x, y, z coordinates
+int atomic_distance (int atom_dist_x, int atom_dist_y, int atom_dist_z); //Defines atomic distance as combination of x, y, z coordinates
 
 int main() {
-//using nedit file...
-//create new input "hannah's test changes"
+	string input_file_name;
+	ifstream input_file;
+
+	//Display prompt for input file name
+	cout << "Please input the name of Gaussian optimization file: ";
+	cin >> input_file_name;
+
+	//Check that input file opens
+	input_file.open(input_file_name.c_str() );
+	if (!input_file.is_open() ) {
+		cout << "Error: Unable to open the input file.";
+		return 1;
+	}
+
+	const string gaussian_output::HEADER_STRING = "OPTIMIZED PARAMETERS";
+	const string gaussian_output::FOOTER_STRING = "GradGradGradGradGradGradGradGradGradGradGrad";
+
+	//Read through the file and for now output the coordinates
+	while (getline(input_file, gaussian_coordinates, FOOTER_STRING))
+	{
+		if (gaussian_coordinates.find(HEADER_STRING) != string::npos)
+		{
+			cout << gaussian_coordinates << "\n";
+		}
+	}
+
 	return 0;
 }
 
