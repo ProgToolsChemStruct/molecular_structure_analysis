0a75acdc4602eb526618e8b09c6e54df61632ace
Thomas Summers <tsmmers1@memphis.edu>
2017-02-21 10:47:56 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,25 @@
 #include "main.h"
 
 using namespace std;
+/**
+ * @mainpage The Gaussian Optimization Analytical Tool (GOAT)
+ *
+ * Welcome to the Gaussian Optimization Analytical Tool (GOAT) documentation site! 
+ * Users may find relevant info related to this program, a program designed to provide
+ * structural analyses of biomolecules successfully optimized using Gaussian software.
+ *
+ * @short   Main program
+ * @file    main.cpp
+ * @author  Kate Charbonnet, Hannah Lozano, and Thomas Summers
+ * @param   none
+ * @return  0 on success
+ *
+ * The purpose of this program is to provide preliminary structural information on biomolecules
+ * optimized using Gaussian computational chemistry software.  Structural and chemical properties 
+ * identified include: element identification, bond length, bond order, central angles, and torsional
+ * angles.  Input of the file to be analyzed will result in an output file listing all the structural
+ * information of the biomolecule.
+ */
 
 int atomic_distance (int atom_dist_x, int atom_dist_y, int atom_dist_z); //Defines atomic distance as combination of x, y, z coordinates
 
