39cb0e3a59b364e2c37c5a801ebb0661a92b06a5
DarmiusX <tsmmers1@memphis.edu>
2017-02-21 01:14:35 -0600

    Revised main.cpp to be compilable

--- main.cpp
+++ main.cpp
@@ -9,32 +9,45 @@ using namespace std;
 int atomic_distance (int atom_dist_x, int atom_dist_y, int atom_dist_z); //Defines atomic distance as combination of x, y, z coordinates
 
 int main() {
-	string input_file_name;
-	ifstream input_file;
+	ifstream inputfile;
+	string line;
+	string header = "OPTIMIZED PARAMETERS";
 
-	//Display prompt for input file name
-	cout << "Please input the name of Gaussian optimization file: ";
-	cin >> input_file_name;
-
-	//Check that input file opens
-	input_file.open(input_file_name.c_str() );
-	if (!input_file.is_open() ) {
+	//Open the input file and check that it opened
+	inputfile.open("gaussian_opt_file");
+	if (!inputfile.is_open() ) {
 		cout << "Error: Unable to open the input file.";
 		return 1;
 	}
-
-	const string gaussian_output::HEADER_STRING = "OPTIMIZED PARAMETERS";
-	const string gaussian_output::FOOTER_STRING = "GradGradGradGradGradGradGradGradGradGradGrad";
-
-	//Read through the file and for now output the coordinates
-	while (getline(input_file, gaussian_coordinates, FOOTER_STRING))
-	{
-		if (gaussian_coordinates.find(HEADER_STRING) != string::npos)
-		{
-			cout << gaussian_coordinates << "\n";
+	
+	//Search the input file
+	while (getline(inputfile, line)) {
+		if (line.find(header, 0) != string::npos) {
+			cout << "Found: " << header << endl;
 		}
 	}
-
+	inputfile.close();
 	return 0;
 }
+
+
+
+
+
+
+
+	//const string gaussian_output::HEADER_STRING = "OPTIMIZED PARAMETERS";
+	//const string gaussian_output::FOOTER_STRING = "GradGradGradGradGradGradGradGradGradGradGrad";
+
+	//Read through the file and for now output the coordinates
+	//while (getline(input_file, gaussian_coordinates, FOOTER_STRING))
+	//{
+		//if (gaussian_coordinates.find(HEADER_STRING) != string::npos)
+		//{
+			//cout << gaussian_coordinates << "\n";
+		//}
+	//}
+
+	//return 0;
+//}
 
