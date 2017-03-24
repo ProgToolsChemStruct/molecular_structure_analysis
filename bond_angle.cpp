9cffed55e57444da6e024091bf4cf7a255eccd3b
Kate Charbonnet <kchrbnnt@memphis.edu>
2017-03-24 10:30:51 -0500

    added spacing between functions

--- bond_angle.cpp
+++ bond_angle.cpp
@@ -6,27 +6,35 @@ using namespace std;
 double ex_ji(double x_j, double x_i, double R_ji) {     
     return ((-(x_j-x_i))/R_ji);
 }
+
 double ey_ji(double y_j, double y_i, double R_ji) {
     return ((-(y_j-y_i))/R_ji);
 }
+
 double ez_ji(double z_j, double z_i, double R_ji) {
     return ((-(z_j-z_i))/R_ji);
 }    
+
 double ex_jk(double x_j, double x_k, double R_jk) {     
     return ((-(x_j-x_k))/R_jk);
 }
+
 double ey_jk(double y_j, double y_k, double R_jk) {
     return ((-(y_j-y_k))/R_jk);
 }
+
 double ez_jk(double z_j, double z_k, double R_jk) {
     return ((-(z_j-z_k))/R_jk);
-}    
+}  
+
 double ex_kl(double x_k, double x_l, double R_kl) {     
     return ((-(x_k-x_l))/R_kl);
 }
+
 double ey_kl(double y_k, double y_l, double R_kl) {
     return ((-(y_k-y_l))/R_kl);
 }
+
 double ez_kl(double z_k, double z_l, double R_kl) {
     return ((-(z_k-z_l))/R_kl);
 }
