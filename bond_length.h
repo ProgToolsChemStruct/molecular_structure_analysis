63b8e6b9ba28723734cbfc70733cbe6d348ba258
Hannah Lozano <hclozano@memphis.edu>
2017-03-27 14:42:28 -0500

    Create bond_length.h

---
+++ bond_length.h
@@ -0,0 +1,34 @@
+#include <iostream>
+
+/** 
+  *@file elements_h
+  *@author Hannah Lozano
+  *@param none
+  *@ return 0 if success
+  *
+  * The prupose of this file is to calculate the distance between atoms and if the distance is less tha 1.55 angstroms they are bonded
+  **/
+  
+using namespace std;
+
+//open coordinates.csv array
+
+int main() 
+{
+using R_ji (x_j, x_i, y_j, y_i, z_j, z_i)
+{
+    return ((sqrt((x_i-x_j) pow 2) + (y_i-y_j) pow 2) + (z_i-z_j) pow 2)))
+    };
+    
+using R_jk (x_k,x_j,y_k,y_j,z_k,z_j) 
+{
+    return ((sqrt((x_j-x_k) pow 2) + (y_j-y_k) pow 2) + (z_j-z_k) pow 2))))
+    };
+       
+using R_kl (x_l,x_k,y_l,y_k,z_l,z_k) 
+{
+    return ((sqrt((x_k-x_l) pow 2) + (y_k-y_l) pow 2) + (z_k-z_l) pow 2)))) 
+    };
+    
+return 0;
+ 
