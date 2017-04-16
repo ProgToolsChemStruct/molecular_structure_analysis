2aad082792b864b679667eb664b5aadcabc8bd1e
Kate Charbonnet <kchrbnnt@memphis.edu>
2017-04-16 12:26:50 -0500

    temporary changes

--- bond_angle.h
+++ bond_angle.h
@@ -14,14 +14,27 @@
 
 using namespace std;
 
-//header content
-class Bond_Angle {
-private:
+void atom_distance(int i, int j, int k, int l, double distance, 
+                   string atom1, string atom2, string atom3, string atom4,
+		   string x1_string, string x2_string, 
+		   string y1_string, string y2_string, 
+		   string z1_string, string z2_string, 
+		   double x1_double, double x2_double,
+		   double y1_double, double y2_double,
+		   double z1_double, double z2_double, 
+		   vector< vector<string> > & vector_coords);
 
-
-public:
-void atom_dist();
-		 
-void angle_phi();
-};	       
+void unit_vects(int i, int j, 
+                double x1_double, double x2_double, 
+		double y1_double, double y2_double, 
+		double z1_double, double z2_double, 
+		vector< vector<double> > & R, 
+		vector< vector<string> > & vector_coords);
+		
+void bond_angle_phi(int i, int j, int k, 
+                    string atom1, string atom2, string atom3, 
+		    vector< vector<double> > & R, 
+		    vector< vector<double> > & unit_vectors, 
+		    vector< vector<string> > & vector_coords);
+      
 #endif //BOND_ANGLE_H
