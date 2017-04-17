c7063745196993048a6d930431d0eb6b3e7e3206
Hannah Caitlin Lozano <hclozano@memphis.edu>
2017-04-17 11:23:19 -0500

    created bond_number.cpp

---
+++ bond_number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include "elements.h"
+
+using namespace std;
+
+int max_bond_number (enum elements_hydrogen_to_argon key){
+    
+    1,0,1,2,3,4,
+    3,2,1,0,1,2,
+    3,4,3,2,1,0;
+    
+    cout << "Max bond number is: " << max_bond_number <<"    "<< endl;
+}    
+
+
