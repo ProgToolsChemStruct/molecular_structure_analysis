d0d7ec1b49abc854ca76d214c78a99bc0b3752bd
Kate Charbonnet <kchrbnnt@memphis.edu>
2017-04-19 15:04:54 -0500

    removed torsion angle stuff

--- bond_angle.h
+++ bond_angle.h
@@ -8,18 +8,15 @@
  * and bond distances. 
  */
 
-//header guard
 #ifndef BOND_ANGLE_H
 #define BOND_ANGLE_H
 
 using namespace std;
 
-void atom_distance(int number_atoms);
+void atom_distance_f(int number_atoms);
 
 double unit_vectors(int a1, int a2, int col);
 		
 void bond_angle_f(int number_atoms);
-
-void torsion_angle_f(int number_atoms);
       
 #endif //BOND_ANGLE_H
