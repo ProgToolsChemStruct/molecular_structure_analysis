ed787718be1f24483c0e1597ed5865804f5ecace
Hannah Caitlin Lozano <hclozano@memphis.edu>
2017-04-19 15:07:44 -0500

    updated so it compiles

--- bond_number.h
+++ bond_number.h
@@ -11,6 +11,15 @@
 //header guard
 #ifndef BOND_NUMBER_H
 #define BOND_NUMBER_H
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <cmath>
+#include <cstdlib>
+
+#include "bond_number.h"
+#include "bond_length.h"
 
 int dist_calc (int i , int j); 
 
@@ -18,12 +27,13 @@ int dist_calc (int i , int j);
  *This function does just the interatomic distance calculations for the bond      *nuumber calculations. 
  *It has a direct return instead of an output to the screen 
  *in the main file.
- */ 
+ */
 
-int bond_number;
+int number_bonds(int dist_calc);
 
 /**
  *This function uses the dist_calc function above to determine the exact number   *of bonds between atoms that are bound to each other.
- */    
+ */
+     
 
 #endif //BOND_NUMBER_H
