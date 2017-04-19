d12f56302d06bc9a5357270b54222ba5393f8ce9
Kate Charbonnet <kchrbnnt@memphis.edu>
2017-04-19 15:07:44 -0500

    creation of torsion_angle.cpp

---
+++ torsion_angle.h
@@ -0,0 +1,18 @@
+/**
+ * @file    torsion_angle.h
+ * @author  Kate Charbonnet
+ * @param   none
+ * @return  0 on success
+ *
+ * The purpose of this program is to calculate torsion angles (tau) using calculated unit vectors
+ * and bond angles. 
+ */
+
+#ifndef TORSION_ANGLE_H
+#define TORSION_ANGLE_H
+
+using namespace std;
+
+void torsion_angle_f(int number_atoms);
+      
+#endif //TORSION_ANGLE_H
