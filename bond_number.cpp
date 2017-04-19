0c104aea1749cd2de6a53cb6eeec86ae809b64b2
Hannah Caitlin Lozano <hclozano@memphis.edu>
2017-04-19 15:07:30 -0500

    updated so it compiles

--- bond_number.cpp
+++ bond_number.cpp
@@ -2,11 +2,10 @@
 #include <string>
 #include <fstream>
 #include <vector>
-#include <cstream>
 #include <cmath>
+#include <cstdlib>
 
 #include "bond_number.h"
-#include "elements.h"
 #include "bond_length.h"
 
 using namespace std;
@@ -18,11 +17,11 @@ extern int i, j; //reference to bond_length
 string x1_string, x2_string, y1_string, y2_string, z1_string, z2_string; //xyz coordinates as strings
 double x1_double, x2_double, y1_double, y2_double, z1_double, z2_double; //xyz coordinates as doubles
 
-int dist_calc (int i , int j) {
+int dist_calc(int i , int j) {
 
             x1_string = (vector_coords[i][2]);
 	    x2_string = (vector_coords[j][2]);
-            y1_string = (vector_coords[i][3]):
+            y1_string = (vector_coords[i][3]);
             y2_string = (vector_coords[j][3]);
             z1_string = (vector_coords[i][4]);
             z2_string = (vector_coords[j][4]);
@@ -42,17 +41,16 @@ int dist_calc (int i , int j) {
 	    return distance; //direct return of distance vs output to screen in bond_length
 }
 
-
-int bond_number (){   
+int number_bonds() {   
     
     ofstream log;
-    log.open("log.txt"; ios::app);
+    log.open("log.txt", ios::app);
     
     cout << "Bond numbers between atoms: " << endl;
      
     for (int i=0; i < vector_coords.size(); i++) {
     int counter = 0;
-        for (int j=i+1; j < vector_coords.size(); j++) {
+        for (int j=i+1; j <vector_coords.size(); j++) {
             if (j == i) continue;
 	    double distance = 0.00;
 	    distance = dist_calc(i,j);
