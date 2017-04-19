c0d4b14d21287519d33f3bbc995a96d7f7951ba9
Kate Charbonnet <kchrbnnt@memphis.edu>
2017-04-19 10:06:37 -0500

    addition of dihedral angle

--- bond_angle.h
+++ bond_angle.h
@@ -19,5 +19,7 @@ void atom_distance(int number_atoms);
 double unit_vectors(int a1, int a2, int col);
 		
 void bond_angle_f(int number_atoms);
+
+void torsion_angle_f(int number_atoms);
       
 #endif //BOND_ANGLE_H
